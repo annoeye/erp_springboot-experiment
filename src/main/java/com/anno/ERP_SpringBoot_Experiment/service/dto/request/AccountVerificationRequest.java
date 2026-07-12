d911f0cba27c9c76e387bacf69a02057d9298921
ddicgegd <ddicgegd@gmail.com>
2026-07-12 21:10:36 +0700

    fix(auth): support both camelCase and snake_case for AccountVerificationRequest

--- src/main/java/com/anno/ERP_SpringBoot_Experiment/service/dto/request/AccountVerificationRequest.java
+++ src/main/java/com/anno/ERP_SpringBoot_Experiment/service/dto/request/AccountVerificationRequest.java
@@ -1,6 +1,7 @@
 package com.anno.ERP_SpringBoot_Experiment.service.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -17,7 +18,8 @@ public class AccountVerificationRequest {
     /**
      * Mật khẩu mới (bắt buộc, ít nhất 6 ký tự).
      */
-    @JsonProperty("new_password")
+    @JsonProperty("newPassword")
+    @JsonAlias("new_password")
     @NotBlank(message = "Mật khẩu mới không được để trống")
     @Size(min = 6, message = "Mật khẩu mới phải có ít nhất 6 ký tự")
     String newPassword;
@@ -25,7 +27,8 @@ public class AccountVerificationRequest {
     /**
      * Xác nhận mật khẩu mới (bắt buộc).
      */
-    @JsonProperty("confirm_password")
+    @JsonProperty("confirmPassword")
+    @JsonAlias("confirm_password")
     @NotBlank(message = "Xác nhận mật khẩu không được để trống")
     String confirmPassword;
 }
